b655eecdb8d9efa79a82d17cdc6f088668fedb51
Laila Campbell <lailacampbell96@gmail.com>
2020-02-19 01:25:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,18 @@
 def oxford_comma(array)
-[kiwi]
+  array = [*array]
+  case array.size
+  when 0
+    ''
+  when 1
+    array[0].to_s
+  when 2
+    array.join(' and ')
+  else
+    array_copy = array
+    array_copy[-1] = "and #{array_copy[-1]}"
+    array_copy.join(', ')
+  end
+end
   
 
 end
